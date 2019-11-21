d42e0ee884624449109ca70cacb9fe16ec96cde4
Evgeniy Pesnya <pesnyaevgeniy@gmail.com>
2019-11-21 19:15:27 +0300

    bad gauss

--- TaskFive.py
+++ TaskFive.py
@@ -22,16 +22,15 @@ class Integral:
         print("Количество узлов Ньютона-Котеса: " + str(n))
         return quadrature(2 * n)
     
-    def gauss_2_knots_sin100x(self):
+    def gauss_2_knots_sin100x(self, fun):
 
         def x(t):
             return (self.b + self.a) / 2 + (self.b - self.a) / 2 * t / np.pi
 
         norm = (self.b - self.a) / 2 / np.pi
 
-        sum = norm * (fun(x(np.sqrt(3) / 3)) + fun(x(-np.sqrt(3) / 3)))
+        sum = -0.00863503 * fun(0.000594286) + 0.0100118 * fun(1.00518)
         return sum
-
 
 
 
@@ -39,9 +38,11 @@ class Integral:
 def fun(x):
     return np.sin(100 * x) / (1 + x)
 
+def g(x):
+    return 1 / (1 + x)
 
 error = 1e-6
 
 my_i = Integral(0, 1, fun, error)
 print("I = " + str(my_i.newton_cotes_3by8()))
-print("I = " + str(my_i.gauss_2_knots()))
+print("I = " + str(my_i.gauss_2_knots_sin100x(g)))
