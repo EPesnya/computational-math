7a9ad0c7c4a00f44a740f3ba5cfa8955ef6606f0
Evgeniy Pesnya <pesnyaevgeniy@gmail.com>
2020-02-08 11:20:23 +0300

    1 sem finish

--- TaskSix.py
+++ TaskSix.py
@@ -3,24 +3,36 @@ import numpy as np
 
 #решение y = pi^2
 
-y = np.pi**2 + 1e-15
-x = 0
-n = 55   # начиная с 55 начинает возростать погрешность
-h = 5 / n
+n = 2   # начиная с 55 начинает возростать погрешность
 
-x_set = np.ndarray(n)
-y_set = np.ndarray(n)
+# x_set = np.ndarray(n)
+# y_set = np.ndarray(n)
 
 def function(x, y):
     return (y - np.pi**2) / (x - 5)**4
 
-for i in range(n):
-    Y = y + h * function(x, y)
-    y += h * (function(x, y) + function(x, Y)) / 2
-    x += h
-    x_set[i] = x
-    y_set[i] = y
-    print(str(x) + " " + str(y))
+while n < 2600:
+    y = np.pi**2 + 1e-15
+    yk = 0
+    x = 0
+    h = 5 / n
 
-#plt.plot(x_set, y_set)
-#plt.show()
+    for i in range(n):
+        x = h * i
+        Y = y + h * function(x, y)
+        y += h * (function(x, y) + function(x, Y)) / 2
+        if i < n - 2:
+            yk = y
+        # x += h
+        # x_set[i] = x
+        # y_set[i] = y
+
+    print("L = " + str(n) + "; h = " + str(h) + "; delta = " + str(yk))
+
+    if n > 100:
+        n += 100
+    else:
+        n += 10
+
+# plt.plot(x_set, y_set)
+# plt.show()
